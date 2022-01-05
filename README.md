16d966bfbeccc3eb5462f9d88ed7fb167289eedf
Nicola <nic.baldi@gmail.com>
2022-01-05 12:34:30 +0100

    jupyter notebooks test.

--- README.md
+++
@@ -1,4 +0,0 @@
-# OCAML intepreters
-
-This is just my experimenting with tokenization, parsing and quick interpreter implementations.  
-Right now there's an interpreter for [arithmetic expression]/(./arithmetic-expression-interpreter) and one for lambda calculus.
--- functional-language/interpreter.ml
+++ functional-language/interpreter.ml
@@ -16,6 +16,8 @@ exception FieldNotFound of string ;;
 
 exception TypeMismatch ;;
 
+exception OutOfRangeIndex ;;
+
 (* syntax level (AST) *)
 type exp =
   | CstInt of int
@@ -35,11 +37,9 @@ type exp =
   | Apply of exp * exp
   | LetRec of id * id * exp * exp
   | Record of (label * exp) list
-  | Select of exp * label ;;
-(*
+  | Select of exp * label
   | Tuple of exp list
-  | TupleGet of exp * exp
-*)
+  | TupleGet of exp * exp ;;
 
 (* runtime *)
 type evT = 
@@ -48,6 +48,7 @@ type evT =
   | Closure of id * exp * evT env
   | RecClosure of id * id * exp * evT env
   | Record of (label * evT) list
+  | Tuple of evT list
   | Unbound ;;
 
 exception EvalError of string ;;
@@ -101,77 +102,75 @@ let if_then_else cond e1 e2 = match (typecheck TBool cond, cond) with
   | (false, Bool a) -> e2
   | _ -> failwith ("runtime error") ;;
 
-let tuple_get_arg arg = match (typecheck TInt arg, arg) with
-  | (true, Int a) -> Int a
-  | _ -> failwith ("runtime error") ;;
-
 let rec record_lookup (Lab l) = function
   | [] -> raise (FieldNotFound ("field " ^ string_of_label (Lab l) ^ " was not found"))
   | (Lab l', v)::xs -> if l' = l then v else record_lookup (Lab l) xs ;;
 
-let rec eval e env = match e with
+let tuple_lookup arg e =
+  let rec tuple_lookup_aux body a i = match body with
+    | [] -> raise OutOfRangeIndex
+    | x::xs -> if a = i then x else tuple_lookup_aux xs a (i + 1)
+  in (match (typecheck TInt arg, arg) with
+    | (true, Int a) -> tuple_lookup_aux e a 1
+    | _ -> failwith ("runtime error")) ;;
+
+let rec eval env = function
   | CstInt n -> Int n
   | CstTrue -> Bool true
   | CstFalse -> Bool false
-  | IsZero e -> is_zero (eval e env)
-  | Eq (e1, e2) -> int_eq (eval e1 env) (eval e2 env)
-  | Sum (e1, e2) -> int_plus (eval e1 env) (eval e2 env)
-  | Sub (e1, e2) -> int_sub (eval e1 env) (eval e2 env)
-  | Times (e1, e2) -> int_mul (eval e1 env) (eval e2 env)
-  | Or (e1, e2) -> bool_or (eval e1 env) (eval e2 env)
-  | And (e1, e2) -> bool_and (eval e1 env) (eval e2 env)
+  | IsZero e -> is_zero (eval env e)
+  | Eq (e1, e2) -> int_eq (eval env e1) (eval env e2)
+  | Sum (e1, e2) -> int_plus (eval env e1) (eval env e2)
+  | Sub (e1, e2) -> int_sub (eval env e1) (eval env e2)
+  | Times (e1, e2) -> int_mul (eval env e1) (eval env e2)
+  | Or (e1, e2) -> bool_or (eval env e1) (eval env e2)
+  | And (e1, e2) -> bool_and (eval env e1) (eval env e2)
   | Den x -> env x
-  | IfThenElse (cond, e1, e2) -> eval (if_then_else (eval cond env) e1 e2) env
-  | Let (x, e1, e2) -> eval e2 (bind env x (eval e1 env))
+  | IfThenElse (cond, e1, e2) -> eval env (if_then_else (eval env cond) e1 e2)
+  | Let (x, e1, e2) -> eval (bind env x (eval env e1)) e2
+  (*
+    Fun (x, e)
+    Returns a Closure; it grabs the declaration environment which appears as
+    the Closure's third parameter.
+  *)
   | Fun (x, e) -> Closure (x, e, env)
-  | Apply (e_fun, e_arg) ->
-    let fclosure = eval e_fun env in (match fclosure with
+  (*
+    Apply (e1, e2)
+    e1: expected to be a Closure or a RecClosure.
+    e2: the argument to be passed as the formal parameter.
+  *)
+  | Apply (e1, e2) ->
+    let fclosure = eval env e1 in (match fclosure with
       (*
         the body is evaluated within an environment which is made
         by fun_dec_env (the environment onto which the function was
-        declared) extended with the binding between "x" and the
+        declared) extended by the binding between "x" and the
         argument evaluated into the current environment.
       *)
-      | Closure (x, e_body, fun_dec_env) -> eval e_body (bind fun_dec_env x (eval e_arg env))
-      | RecClosure (g, x, e_body, fun_dec_env) ->
-        let env_arg = bind fun_dec_env x (eval e_arg env) in
+      | Closure (x, e, fun_dec_env) -> eval (bind fun_dec_env x (eval env e2)) e
+      | RecClosure (g, x, e, fun_dec_env) ->
+        let env_arg = bind fun_dec_env x (eval env e2) in
         let env_arg_g = bind env_arg g fclosure in
-        eval e_body env_arg_g
+        eval env_arg_g e
       | _ -> failwith ("expected closure"))
   (*
     for recursive functions a new environment is built:
     it contains an entry of type RecClosure for "f" itself.
   *)
-  | LetRec (f, x, e1, e2) -> eval e2 (bind env f (RecClosure (f, x, e1, env)))
-  | Record body -> Record (eval_record body env)
-  | Select (e, l) -> match eval e env with
-    | Record body -> record_lookup l body
-    | _ -> raise TypeMismatch
-(*
-  | Tuple body -> match body with
-    | [] -> []
-    | x::xs -> (eval x env)::xs
-  | TupleGet (e_tuple, e_arg) ->
-    let arg = tuple_get_arg (eval e_arg env) in 
-*)
-and eval_record body env = match body with
-  | [] -> []
-  | (Lab l, e)::xs -> (Lab l, eval e env)::eval_record xs env
-;;
+  | LetRec (f, x, e1, e2) -> eval (bind env f (RecClosure (f, x, e1, env))) e2
+  | Tuple e -> Tuple (eval_tuple env e)
+  | TupleGet (e1, e2) -> (match eval env e1 with
+    | Tuple e -> tuple_lookup (eval env e2) e
+    | _ -> raise TypeMismatch)
+  | Record e -> Record (eval_record env e)
+  | Select (e, l) -> (match eval env e with
+    | Record e -> record_lookup l e
+    | _ -> raise TypeMismatch)
 
-let x = ("a", 2) in
-let y = snd x + 1 in
-print_int ;;
-(*
-  let x = ("a", 2)
-  Let(
-    "x",
-    Tuple(
-      ["a", 1]
-    ),
-    Apply(
-      Den("print_int"),
-      Den("x")
-    )
-  )
- *)
+and eval_record env = function
+  | [] -> []
+  | (Lab l, e)::xs -> (Lab l, eval env e)::eval_record env xs
+and eval_tuple env = function
+  | [] -> []
+  | x::xs -> (eval env x)::eval_tuple env xs
+;;
